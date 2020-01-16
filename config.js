7847637e46282a9117ed06cda488e71ea1306c29
ishowfun <ishowfun@qq.com>
2020-01-16 09:38:50 +0800

    self change

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ var requirejs = ({
 		'jquery.scrollto': {deps: ['jquery'],exports: 'jQuery'}
 	},
 	paths: {
-		'jquery': 'http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min',
-		'jquery.ui': 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min',
+		'jquery': 'lib/jquery/jquery-1.8.2.min',
+		'jquery.ui': 'lib/jquery/jquery-ui.min',
 		'jquery.scrollto': 'lib/jquery/jquery.scrollTo.min',
-		'underscore': 'http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.1/underscore-min',
-		'knockout':'http://cdnjs.cloudflare.com/ajax/libs/knockout/2.1.0/knockout-min',
+		'underscore': 'lib/underscore/underscore',
+		'knockout':'lib/knockout/knockout-2.1.0',
 
 		'text': 'lib/require/text',
 		'domready': 'lib/require/domready'
